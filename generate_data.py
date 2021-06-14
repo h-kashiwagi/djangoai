105de062e981468a1d6f600c17037a870277bdf4
Kashiwagi Hiromitu <shi10intro@gmail.com>
2021-06-15 07:42:48 +0900

    画像フォルダ

--- generate_data.py
+++ generate_data.py
@@ -1,19 +1,19 @@
 #モジュールのインポート
-from PIL import Image #Imageスラス
+from PIL import Image #Imageクラス
 import os
 import glob
 import numpy as np
-from sklearn.model_selection import train_test_split#データをスプリットする
+from sklearn import model_selection #データをスプリットする
 
 #パラメーターの初期化
-classes = ["clothes","apples"]  #リストを作成
+classes = ["car","motorbike"]  #リストを作成
 num_classes = len(classes) #clothesの文字の長さ
 image_size = 150
 
 
 #画像の読み込みとNumPy配列への変換
-X = []
-Y = []
+X = [] #リスト
+Y = [] #リスト
 
 for index,classlabel in enumerate(classes):
     photos_dir = "./" + classlabel
@@ -22,16 +22,16 @@ for index,classlabel in enumerate(classes):
         image = Image.open(file)
         image = image.convert("RGB")
         image = image.resize((image_size,image_size))
-        data = np.asarray(image,dtype=float)  #255.0で割ったので浮動小数点数になり、ファイルのサイズが大きくなっている
+        data = np.asarray(image) /255.0 #255.0で割ったので浮動小数点数になり、ファイルのサイズが大きくなっている
         #imageは配列（リスト）になっている？
 
         X.append(data)
         Y.append(index)
 
-X = np.array(X,dtype=float)
-Y = np.array(Y,dtype=float)
+X = np.array(X)
+Y = np.array(Y)
 
-X_train, X_test, y_train, y_test = train_test_split(X,Y)  #サーキットラーンを使ってトレーニングしている
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X,Y)  #サーキットラーンを使ってトレーニングしている
 xy = (X_train, X_test, y_train, y_test) #タプル型
 np.save("./imagefile.npy", xy) #バイナリーファイルに変換する
 
